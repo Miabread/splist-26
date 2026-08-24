144b33e790b04ea345f6c514f466cfe48c567294
Miabread <miabreadloaf@gmail.com>
2026-08-24 13:47:51 -0400

    Implement bones of connection pane

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,18 +2,24 @@
 import { Icon } from '@iconify/vue';
 import { onClickOutside } from '@vueuse/core';
 import { ref } from 'vue';
-import { range } from './util';
+import { cn, range } from './util';
 
 const isConnectionsOpen = ref(false);
-const sidebarRef = ref(null);
+const connectionsPaneRef = ref(null);
+const clientPaneRef = ref(null);
 
 const toggleConnectionsPane = () => {
     isConnectionsOpen.value = !isConnectionsOpen.value;
 };
 
-onClickOutside(sidebarRef, () => {
-    if (isConnectionsOpen.value) isConnectionsOpen.value = false;
-});
+onClickOutside(
+    connectionsPaneRef,
+    () => {
+        console.log('meow');
+        if (isConnectionsOpen.value) isConnectionsOpen.value = false;
+    },
+    { ignore: [clientPaneRef] },
+);
 </script>
 
 <template>
@@ -35,13 +41,13 @@ onClickOutside(sidebarRef, () => {
         </nav>
 
         <header
-            class="col-[2/2] row-[1/1] border-l border-b border-mist-700 p-4 flex gap-2.5 items-center rounded-bl-lg bg-mist-800"
+            class="col-[2/2] row-[1/1] border-l border-b border-mist-700 p-4 flex gap-2.5 items-center rounded-l-lg bg-mist-800"
         >
             <div class="bg-orange-300 rounded-lg w-5 h-5 grid place-items-center text-mist-800 text-xs"></div>
             Server Name
         </header>
 
-        <div
+        <aside
             class="col-[2/2] row-[2/4] flex-1 p-4 gap-2.5 flex flex-col border-l border-mist-700 bg-mist-900 rounded-tl-lg"
         >
             <span class="flex gap-2.5 items-center"> <Icon icon="lucide:list-tree" inline /> Threads </span>
@@ -56,18 +62,18 @@ onClickOutside(sidebarRef, () => {
                     Thread {{ j }}
                 </span>
             </template>
-        </div>
+        </aside>
 
         <header class="col-[3/3] row-[1/1] border-l border-b border-mist-700 p-4 flex gap-2.5 items-center bg-mist-800">
             <div class="bg-mist-700 rounded-lg w-5 h-5 grid place-items-center text-xs">#</div>
             Thread Name
         </header>
 
-        <div class="col-[3/3] row-[2/4] p-4 border-l border-mist-700 bg-mist-900">Main content</div>
+        <main class="col-[3/3] row-[2/4] p-4 border-l border-mist-700 bg-mist-900">Main content</main>
 
         <header class="col-[4/4] row-[1/1] border-l border-b border-mist-700 p-4 bg-mist-800"></header>
 
-        <div class="col-[4/4] row-[2/4] p-4 gap-2.5 flex flex-col border-l border-mist-700 bg-mist-900">
+        <aside class="col-[4/4] row-[2/4] p-4 gap-2.5 flex flex-col border-l border-mist-700 bg-mist-900">
             <div class="flex items-center gap-2.5" v-for="i in range(7)" :key="i">
                 <div
                     class="h-8 w-8 rounded-full ring-1 ring-mist-700 bg-blue-300 grid place-items-center text-mist-800 text-base"
@@ -78,42 +84,52 @@ onClickOutside(sidebarRef, () => {
                     <span class="text-xs text-mist-300">Super {{ i }} status</span>
                 </span>
             </div>
-        </div>
+        </aside>
 
-        <div
-            class="col-[1/3] row-[3/3] rounded-lg border border-mist-700 bg-mist-800 mx-2.5 mb-2.5 gap-5 px-2.5 drop-shadow-2xl flex flex-row-reverse items-center"
+        <footer
+            ref="clientPaneRef"
+            :class="
+                cn(
+                    'col-[1/3] row-[3/3] rounded-b-lg border border-mist-700 bg-mist-800 mx-2.5 mb-2.5 gap-5 px-2.5 drop-shadow-2xl flex flex-row-reverse items-center',
+                    !isConnectionsOpen && 'rounded-t-lg',
+                )
+            "
         >
             <Icon icon="lucide:settings" class="h-5 w-5" />
             <Icon icon="lucide:file-chart-column" class="h-5 w-5" />
             <Icon icon="lucide:telescope" class="h-5 w-5" />
 
-            <div
-                class="mr-auto flex flex-row-reverse space-x-reverse -space-x-3 overflow-hidden items-center drop-shadow-2xl"
+            <button
+                @click="toggleConnectionsPane"
+                class="mr-auto flex -space-x-3 overflow-hidden items-center drop-shadow-2xl rounded-lg hover:bg-mist-700 active:bg-mist-600 py-1 px-2.5"
             >
+                <span class="h-5 w-5 flex items-center justify-center text-center mr-1">
+                    <Icon icon="lucide:radio-tower" class="h-5 w-5" />
+                </span>
+
                 <div
                     v-for="i in range(4)"
                     :key="i"
-                    class="h-8 w-8 rounded-full border border-mist-700 bg-blue-300 grid place-items-center text-mist-800 text-base drop-shadow-2xl"
+                    class="h-8 w-8 rounded-full border border-mist-700 bg-blue-300 grid place-items-center text-mist-800 text-base"
                 ></div>
+            </button>
+        </footer>
 
-                <div class="h-10 w-10 rounded-full border border-mist-700 bg-mist-700 drop-shadow-2xl">
-                    <button
-                        @click="toggleConnectionsPane"
-                        class="h-full w-full flex items-center justify-center text-center"
-                    >
-                        <Icon icon="lucide:radio-tower" class="h-5 w-5" />
-                    </button>
-                </div>
-            </div>
-        </div>
+        <aside
+            v-if="isConnectionsOpen"
+            ref="connectionsPaneRef"
+            class="col-[1/3] row-[2/2] rounded-t-lg border-x border-t border-mist-700 bg-mist-800 mx-2.5 mt-10 drop-shadow-2xl"
+        >
+            meow
+        </aside>
 
-        <div
+        <footer
             class="col-[3/3] row-[3/3] rounded-lg border border-mist-700 bg-mist-800 mx-2.5 mb-2.5 px-2.5 drop-shadow-2xl flex items-center"
         >
             <span class="text-mist-300 flex items-center">Message Thread 1</span>
-        </div>
+        </footer>
 
-        <div
+        <footer
             class="col-[4/4] row-[3/3] rounded-lg border border-mist-700 bg-mist-800 mx-2.5 mb-2.5 px-2.5 drop-shadow-2xl flex items-center gap-2.5"
         >
             <div
@@ -124,6 +140,6 @@ onClickOutside(sidebarRef, () => {
                 <span>Username</span>
                 <span class="text-xs text-mist-300">Super cool status</span>
             </span>
-        </div>
+        </footer>
     </div>
 </template>
